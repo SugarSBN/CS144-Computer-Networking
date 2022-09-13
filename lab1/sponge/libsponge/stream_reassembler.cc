60a4a073d63c9b3c1db1ad692479bcea2929d676
SugarSBN <15952265117@126.com>
2022-09-13 09:31:08 +0800

    lab1 primitive method, timed out

--- lab1/sponge/libsponge/stream_reassembler.cc
+++ lab1/sponge/libsponge/stream_reassembler.cc
@@ -1,5 +1,13 @@
+/*
+ * @Author: SuBonan
+ * @Date: 2022-09-09 10:10:24
+ * @LastEditTime: 2022-09-12 11:08:47
+ * @FilePath: \lab1\sponge\libsponge\stream_reassembler.cc
+ * @Github: https://github.com/SugarSBN
+ * これなに、これなに、これない、これなに、これなに、これなに、ねこ！ヾ(*´∀｀*)ﾉ
+ */
 #include "stream_reassembler.hh"
-
+#include <iostream>
 // Dummy implementation of a stream reassembler.
 
 // For Lab 1, please replace with a real implementation that passes the
@@ -12,13 +20,50 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
-StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity), _capacity(capacity) {}
+StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity), _capacity(capacity) {
+    startAt = 0;
+    st = 0;
+    storage = vector<char>();
+    assembled = vector<bool>();
+    for (size_t i = 0;i < capacity;i++) storage.push_back('\0');
+    for (size_t i = 0;i < capacity;i++) assembled.push_back(false);
+    _eofIndex = _capacity + 1;
+}
 
 //! \details This function accepts a substring (aka a segment) of bytes,
 //! possibly out-of-order, from the logical stream, and assembles any newly
 //! contiguous substrings and writes them into the output stream in order.
 void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
-    DUMMY_CODE(data, index, eof);
+    string data1 = data;
+    size_t index1 = index;
+    if (index < startAt) {
+        if (startAt - index >= data.length()) return;
+        index1 = startAt;
+        data1 = data.substr(startAt - index);
+    } 
+
+    size_t lft = _capacity - _output.buffer_size();
+    while (storage.size() - st < lft) {
+        storage.push_back('\0');
+        assembled.push_back(false);
+    }
+
+    size_t l = min(data1.length(), lft - (index1 - startAt));
+
+    for (size_t i = 0;i < l;i++)    {
+        storage[i + index1 - startAt + st] = data1[i];
+        assembled[i + index1 - startAt + st] = true;
+    }
+    size_t maxpre = st;
+    for (;maxpre < assembled.size() && assembled[maxpre] == true;maxpre++);
+    string putin = "";
+    for (size_t i = st;i < maxpre;i++)   putin = putin + storage[i];
+    startAt = startAt + maxpre - st;
+    st = maxpre;
+    _output.write(putin);   
+    
+    if (eof) _eofIndex = data.size() + index;
+    if (startAt == _eofIndex) _output.end_input();
 }
 
 size_t StreamReassembler::unassembled_bytes() const { return {}; }
--- lab1/sponge/libsponge/stream_reassembler.hh
+++ lab1/sponge/libsponge/stream_reassembler.hh
@@ -1,3 +1,11 @@
+/*
+ * @Author: SuBonan
+ * @Date: 2022-09-09 10:10:24
+ * @LastEditTime: 2022-09-12 10:03:20
+ * @FilePath: \lab1\sponge\libsponge\stream_reassembler.hh
+ * @Github: https://github.com/SugarSBN
+ * これなに、これなに、これない、これなに、これなに、これなに、ねこ！ヾ(*´∀｀*)ﾉ
+ */
 #ifndef SPONGE_LIBSPONGE_STREAM_REASSEMBLER_HH
 #define SPONGE_LIBSPONGE_STREAM_REASSEMBLER_HH
 
@@ -5,15 +13,20 @@
 
 #include <cstdint>
 #include <string>
+#include <vector>
 
 //! \brief A class that assembles a series of excerpts from a byte stream (possibly out of order,
 //! possibly overlapping) into an in-order byte stream.
 class StreamReassembler {
   private:
     // Your code here -- add private members as necessary.
-
+    size_t startAt = 0;
+    size_t st = 0;
+    std :: vector<char> storage = std :: vector<char> ();
+    std :: vector<bool> assembled = std :: vector<bool>();
     ByteStream _output;  //!< The reassembled in-order byte stream
     size_t _capacity;    //!< The maximum number of bytes
+    size_t _eofIndex = 0;
 
   public:
     //! \brief Construct a `StreamReassembler` that will store up to `capacity` bytes.
